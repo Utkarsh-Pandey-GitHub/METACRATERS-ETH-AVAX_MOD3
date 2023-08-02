a1fedef283d3b3cc8e0159330adbc1bcd593ae7a
root <root@AUDACE-1>
2023-08-02 15:30:32 +0530

    code completed

--- contracts/MetacrafterToken.sol
+++ contracts/MetacrafterToken.sol
@@ -1,16 +1,33 @@
-// contracts/GLDToken.sol
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    uint256 public initialSupply=10000;
-    constructor() ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
+
+    uint256 private _totalSupply;
+    address public owner;
+    mapping(address => uint256) private _balances;
+
+    constructor(uint256 initialSupply) ERC20("Token", "UTK") {
+        _totalSupply = initialSupply;
+        owner = msg.sender;
+        _mint(owner, initialSupply);
     }
 
-    function giveValue()public view returns(uint256){
-        return initialSupply;
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
     }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        address sender = _msgSender();
+        _transfer(sender, to, value);
+        return true;
+    } 
+
 }
